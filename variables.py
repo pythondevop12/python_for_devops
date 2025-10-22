8c40c51461f38682cc543c6830fa6254c09ab663
pythondevop <70486657+pythondevop12@users.noreply.github.com>
2025-10-22 13:00:02 +0530

    Variables.py
    variables and type check in python

---
+++ variables.py
@@ -0,0 +1,27 @@
+# variable and type checking
+dev1 = "hello world"
+num = 1
+boolee = True
+print(dev1)
+print(type(dev1))
+print(num)
+print(type(num))
+print(boolee)
+print(type(boolee))
+
+
+li = ['ar','pr','mango','orange']
+print(li)
+print(type(li))
+
+tup = ('mango', 'orange','banana','apple')
+print(tup)
+print(type(tup))
+
+se = {'mango', 'orange','banana','apple'}
+print(se)
+print(type(se))
+
+dic = {"name":"kisan","age":28,"place":"Delhi"}
+print(dic)
+print(type(dic))
